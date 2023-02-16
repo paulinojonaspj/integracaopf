febb1f8d53fc819a3312e4e0bdc2e00261d9364f
Paulino Jonas <37410557+paulinojonaspj@users.noreply.github.com>
2023-02-16 17:04:38 +0000

    geração dos valores

--- Aplicacao/src/app/app-routing.module.ts
+++ Aplicacao/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login.component';
 import { RegistarComponent } from './componentes/registar/registar.component';
+import { ApostarComponent } from './componentes/apostar/apostar.component';
 
 const routes: Routes = [
- {path:'', component:LoginComponent},
- {path:'registar', component:RegistarComponent}
+  { path: '', component: ApostarComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registar', component: RegistarComponent }
 ];
 
 @NgModule({
--- Aplicacao/src/app/componentes/apostar/apostar.component.ts
+++ Aplicacao/src/app/componentes/apostar/apostar.component.ts
@@ -12,4 +12,29 @@ export class ApostarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  numbers?: string;
+  stars?: string;
+
+  generateNumbers(): void {
+    let numbersArray = [];
+    while (numbersArray.length < 5) {
+      let random = Math.floor(Math.random() * 50) + 1;
+      if (numbersArray.indexOf(random) === -1) {
+        numbersArray.push(random);
+      }
+    }
+    numbersArray.sort((a, b) => a - b);
+    this.numbers = numbersArray.join(', ');
+
+    let starsArray = [];
+    while (starsArray.length < 2) {
+      let random = Math.floor(Math.random() * 12) + 1;
+      if (starsArray.indexOf(random) === -1) {
+        starsArray.push(random);
+      }
+    }
+    starsArray.sort((a, b) => a - b);
+    this.stars = starsArray.join(', ');
+  }
+
 }
